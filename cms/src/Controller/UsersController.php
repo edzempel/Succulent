e4840d23b53c35c9f76aed0b67506853e489e38d
Ed <Zempel>
2019-06-03 22:46:01 -0500

    added login functionality

--- cms/src/Controller/UsersController.php
+++ cms/src/Controller/UsersController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
@@ -102,5 +103,17 @@ class UsersController extends AppController
         }
 
         return $this->redirect(['action' => 'index']);
+    }
+
+    public function login()
+    {
+        if ($this->request->is('post')) {
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            }
+            $this->Flash->error('Your username or password is incorrect.');
+        }
     }
 }
