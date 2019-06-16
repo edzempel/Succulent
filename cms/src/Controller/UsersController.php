685ce9f83c22e0e13f6196203ec2a0c191bceb6b
edzempel <edward.zempel@gmail.com>
2019-06-16 14:43:23 -0500

    removed references to articles in user files

--- cms/src/Controller/UsersController.php
+++ cms/src/Controller/UsersController.php
@@ -34,7 +34,7 @@ class UsersController extends AppController
     public function view($id = null)
     {
         $user = $this->Users->get($id, [
-            'contain' => ['Articles']
+            'contain' => ['Plants']
         ]);
 
         $this->set('user', $user);
--- cms/src/Model/Entity/User.php
+++ cms/src/Model/Entity/User.php
@@ -33,7 +33,7 @@ class User extends Entity
         'username' => true,
         'created' => true,
         'modified' => true,
-        'articles' => true
+        //'articles' => true
     ];
 
     /**
