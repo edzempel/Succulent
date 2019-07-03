b6d6b35ee5aac6bccbe7523de624dd1998ab632d
edzempel <edward.zempel@gmail.com>
2019-07-03 11:14:33 -0500

    Fixed 'edit plant'
    removed FindBySlug(), the view was passing the plant->id instead of the slug so I changed the Plant->edit() to used $id instead

--- cms/src/Controller/PlantsController.php
+++ cms/src/Controller/PlantsController.php
@@ -81,10 +81,9 @@ class PlantsController extends AppController
      * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function edit($slug)
+    public function edit($id)
     {
-        $plant = $this->Plants->findBySlug($slug)
-            ->contain([])->firstOrFail();
+        $plant = $this->Plants->get($id);
 
         if ($this->request->is(['patch', 'post', 'put'])) {
             $plant = $this->Plants->patchEntity($plant, $this->request->getData(), ['accessibleFields' => ['user_id' => false]]);
