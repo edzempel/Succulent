d1c84270fcaec95746c9fcbdbd8c8f26e796b1ba
edzempel <edward.zempel@gmail.com>
2019-08-13 20:04:21 -0500

    baked LogoHelper

---
+++ cms/src/View/Helper/LogoHelper.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\View\Helper;
+
+use Cake\View\Helper;
+use Cake\View\View;
+
+/**
+ * Logo helper
+ */
+class LogoHelper extends Helper
+{
+    /**
+     * Default configuration.
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [];
+
+}
